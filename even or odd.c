99b01a8c23d2435b10d683f328d683b64e11ac56
poojasrijasti <231FA04767@gmail.com>
2025-03-07 11:42:11 +0530

    Update even or odd.c

--- even or odd.c
+++ even or odd.c
@@ -3,8 +3,7 @@ int main() {
     int num;
     printf("Enter an integer: ");
     scanf("%d", &num);
-
-    // true if num is perfectly divisible by 2
+    
     if(num % 2 == 0)
         printf("%d is even.", num);
     else
